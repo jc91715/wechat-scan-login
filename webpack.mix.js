11ca2cf09400a9f81b254017bff1a3ba1351eb27
jc91715 <370234915@qq.com>
2019-02-23 15:03:36 +0800

    v1

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,16 @@
-let mix = require('laravel-mix');
-mix
-    .js('resources/js/common/bootstrap.js', 'js/bootstrap.js')
-    .js('resources/js/front/app.js', 'js/app.js')
-    .autoload({
-        jquery: ['$', 'window.jQuery', 'jQuery']
-    })
-    .sass('resources/sass/app.scss', 'css/')
-    .extract(['vue', 'vue-router', 'axios','jquery','lodash']) // 提取依赖库
-    .version()
-    .setResourceRoot('/build/') // 设置资源目录
-    .setPublicPath('./public/build') // 设置 mix-manifest.json 目录
 // let mix = require('laravel-mix');
 // mix
-//     .js('resources/js/app.js', 'js/')
+//     .js('resources/js/common/bootstrap.js', 'js/bootstrap.js')
+//     .js('resources/js/front/app.js', 'js/app.js')
+//     .autoload({
+//         jquery: ['$', 'window.jQuery', 'jQuery']
+//     })
 //     .sass('resources/sass/app.scss', 'css/')
+//     .extract(['vue', 'vue-router', 'axios','jquery','lodash']) // 提取依赖库
+//     .version()
+//     .setResourceRoot('/build/') // 设置资源目录
+//     .setPublicPath('./public/build') // 设置 mix-manifest.json 目录
+let mix = require('laravel-mix');
+mix
+    .js('resources/js/app.js', 'js/')
+    .sass('resources/sass/app.scss', 'css/')
