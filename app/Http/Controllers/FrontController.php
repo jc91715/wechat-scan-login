31ebaf6f1a90753ad53ff55d8f4ff8ea0300539e
jc91715 <370234915@qq.com>
2019-02-23 14:02:32 +0800

    js

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -21,7 +21,7 @@ class FrontController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function home()
     {
         return view('front.home');
     }
